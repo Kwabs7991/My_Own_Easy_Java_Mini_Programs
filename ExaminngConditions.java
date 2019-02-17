c8ea552ec13e232cdae436919f22f110e848bb48
Mike K T Tabi <42524675+Kwabs7991@users.noreply.github.com>
2019-02-17 20:51:56 +0000

    Update ExaminngConditions.java
    This is the ? : operator, also known as the "Ternary" operator which is used to the evaluate the expression, looking for a true or false value then returning one of the two written operands.  This would depend on the result of the evaluation.
    
    The format of the Ternary Operator is as follows: 
    
    ( Boolean-expression )  ?  if-true-return-this  :  if-false-return-this ;
    
    
    Example:
    
    - Thanos = (WisdomWords)  ?  "Sacrifice and stuff"  :  "Keep Conquering planets and stuff" ;
    
    ++ When the variable "WisdomWords" is true the conditional Ternary Operator will assign the value held in the 1st operand ( which is "Sacrifice and Stuff" ) to the Thanos variable, otherwise it then assigns the 2nd operand ( which is "Keep Conquering and stuff" ) to the Thanos operator instead.

--- ExaminngConditions.java
+++ ExaminngConditions.java
@@ -1,1 +1,24 @@
+class Condition{
 
+	public static void main (String args[]){
+	
+	
+	
+		//Declaring and initializing variables.
+		int numbre1 =  1234;
+		int numbre2 =  4325;
+		string endResult;
+		
+		
+		//Ternary Operator used to determine if the integer value is odd or even.
+		//The expression will evaluate true when there are any remainders.
+		result = ( numbre1 % 2 != 0 ) ? "Odd" : "Even";
+		System.out.println(numbre1 + " is "  + result);
+		
+		result = ( numbre2 % 2 != 0 ) ? "Odd" : "Even";
+		System.out.println(numbre2 + " is "  + result);
+	
+	
+	}
+
+}
