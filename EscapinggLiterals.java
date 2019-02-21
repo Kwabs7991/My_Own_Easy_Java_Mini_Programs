de8f5c5020b87df94c91909957d8bf4927620e67
Mike K T Tabi <42524675+Kwabs7991@users.noreply.github.com>
2019-02-21 01:51:44 +0000

    Update EscapinggLiterals.java
    The numerical and text values in Java programs are known as "Literals". Literals are where double or sinlge quotes are required when typing string and that they are to be treated literally to avoid ant permanent termination of the string. This is done through the use of the "\" escape operator. For example: 
    
    String SayWords =  "\"Say more words." She said angrily".
    
    Escape sequences for Output Formatting:-
    
    \n - Newline
    \t - Tab
    \b - Backspace
    \r - Carriage return
    \f - Formfeed
    \\ - Backlash
    \' - Single quote marks
    \" - Double quote marks

--- EscapinggLiterals.java
+++ EscapinggLiterals.java
@@ -1,1 +1,18 @@
-
+public class MikeMomments
+{
+    public static void main (String args[]){
+    
+        String header = "\n\tMIKE'S LIFE MOMENTS:\n";
+         header += "\n\tYear\t\tGood/Bad\t?/10\tMike Moment\n";
+         header += "\t----\t\t--------\t----\t-----------\n";
+         
+         String lifeLesson = "\t2000\t\tGood\t\t7\tSurvived Y2K\n";
+         lifeLesson += "\t2015\t\tBad\t\t8\tFlopped Exams, damn\n"; 
+         lifeLesson += "\t2016\t\tGood\t\t10\tFirst GF, mad\n"; 
+         lifeLesson += "\t2019\t\tBad\t\t10\tStubbed my Toe\n"; 
+         
+         System.out.print(header + lifeLesson);
+       
+    
+    }
+}
